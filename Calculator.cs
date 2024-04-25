b28a4b3fd6f6421eec9fd7d065aa919011f16450
mrmax <mrmax@DESKTOP-789VVGH>
2024-04-25 20:01:36 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Calculator<T>
+    {
+        public T Add(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 + num2;
+        }
+
+        public T Subtract(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 - num2;
+        }
+
+        public T Multiply(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 * num2;
+        }
+
+        public T Divide(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль.");
+            }
+            return num1 / num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<int> intCalculator = new Calculator<int>();
+            Console.WriteLine("Сложение (5 + 3) = " + intCalculator.Add(5, 3));
+            Console.WriteLine("Вычитание (5 - 3) = " + intCalculator.Subtract(5, 3));
+            Console.WriteLine("Умножение (5 * 3) = " + intCalculator.Multiply(5, 3));
+            Console.WriteLine("Деление (5 / 3) = " + intCalculator.Divide(5, 3));
+
+            Calculator<double> doubleCalculator = new Calculator<double>();
+            Console.WriteLine("\nСложение (5.5 + 3.5) = " + doubleCalculator.Add(5.5, 3.5));
+            Console.WriteLine("Вычитание (5.5 - 3.5) = " + doubleCalculator.Subtract(5.5, 3.5));
+            Console.WriteLine("Умножение (5.5 * 3.5) = " + doubleCalculator.Multiply(5.5, 3.5));
+            Console.WriteLine("Деление (5.5 / 3.5) = " + doubleCalculator.Divide(5.5, 3.5));
+            Console.ReadKey();
+        }
+    }
+}
+    
+
